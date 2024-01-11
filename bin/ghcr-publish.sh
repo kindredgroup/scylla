100064d3daf402519e04a8c185ecd0496193621c
Amninder Kaur <36900343+akaur13@users.noreply.github.com>
2024-01-11 17:35:52 +1100

    fix: version and tag for package while building docker image  and  pushing to ghcr io (#6)
    Co-authored-by: Amninder Kaur <amninder.kaur@kindredgroup.com>

--- bin/ghcr-publish.sh
+++ bin/ghcr-publish.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
 set -e
 package_name=$1
-docker_file=Dockerfile.$package_name
+tag=$2
+if [ -z "$tag" ]; then
+   tag="$package_name"
+fi
+docker_file=Dockerfile.$tag
+cargo pkgid -p $package_name # to check if the package name is correct
 version=$(cargo pkgid -p $package_name | cut -d# -f2 | cut -d: -f2)
-package_version=$package_name:v$version
-tag_ref=ghcr.io/kindredgroup/scylla/$package_version
+tag_with_version=$tag:v$version
+tag_ref=ghcr.io/kindredgroup/scylla/$tag_with_version
 echo $tag_ref
 docker build -f $docker_file . --tag $tag_ref
 #for ghcr.io access token mentioned in the github secrets and accessed in actions
