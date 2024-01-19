e27f9df6eeeeb7773e2aee45648d1966994ec656
Amninder Kaur <36900343+akaur13@users.noreply.github.com>
2024-01-19 11:14:55 +1100

    fix: disable the create scylla DB step from migrations (#7)

--- bin/component-test-setup.sh
+++ bin/component-test-setup.sh
@@ -6,6 +6,7 @@ echo PG_PASSWORD=admin >> .env
 echo PG_DATABASE=scylla >> .env
 echo PG_PORT=5432 >> .env
 echo PG_POOL_SIZE=10 >> .env
+echo DB_ENV=mig-test >> .env
 # run db-migrations
 make withenv RECIPE=db.migrate
 
